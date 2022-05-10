87808083137f01bc476ec1787fbc91b48c0a4691
Mykyta Vostrikov <mykyta.vostrikov@sigma.software>
2022-05-10 16:29:43 +0300

    Precommit hook configuration done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,45 @@
 module.exports = {
   extends: [
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended',
-    'plugin:import/recommended',
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/recommended",
     "eslint:recommended",
+    "plugin:react/recommended",
   ],
-  plugins: ['react', '@typescript-eslint', 'jest', 'import'],
+  plugins: ["react", "@typescript-eslint", "jest", "import"],
   env: {
     browser: true,
     es6: true,
     jest: true,
   },
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
-    project: './tsconfig.json',
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
   rules: {
-    'linebreak-style': 'off',
-    'prettier/prettier': [
-      'error',
+    "linebreak-style": "off",
+    "prettier/prettier": [
+      "error",
       {
-        endOfLine: 'auto',
+        endOfLine: "auto",
       },
     ],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "./App.module.scss";
 
 const App = () => {
