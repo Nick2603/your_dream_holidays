580455c86fccde619b7c875d67a9cabe2cdce1be
Mykyta Vostrikov <mykyta.vostrikov@sigma.software>
2022-05-11 11:53:26 +0300

    test

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import {Router} from 'react-router-dom';
-
 import styles from "./App.module.scss";
 
 const App = () => {
   return <div className={styles.div}>Your Dream Holidays</div>;
 };
-
 
 export default App;
