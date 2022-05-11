4f060c64c0659026eea3ff567786467daa41c2e1
Mykyta Vostrikov <mykyta.vostrikov@sigma.software>
2022-05-11 14:36:53 +0300

    Final config

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,8 @@
+import React from "react";
 import styles from "./App.module.scss";
 
 const App = () => {
   return <div className={styles.div}>Your Dream Holidays</div>;
 };
-
-
-
-
 
 export default App;
