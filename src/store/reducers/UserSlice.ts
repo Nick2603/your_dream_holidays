14659569fcaf6c6b294a60e34455ffe77cb35210
Mykyta Vostrikov <mykyta.vostrikov@sigma.software>
2022-05-12 14:32:06 +0300

    Redux config

---
+++ src/store/reducers/UserSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IUser } from "../../models/IUser";
+
+interface UserState {
+  users: IUser[];
+  isLoading: boolean;
+  error: string;
+  count: number;
+}
+
+const initialState: UserState = {
+  users: [],
+  isLoading: false,
+  error: "",
+  count: 0,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    increment(state, action: PayloadAction<number>) {
+      state.count += action.payload;
+    },
+    decrement(state, action: PayloadAction<number>) {
+      state.count -= action.payload;
+    },
+  },
+});
+
+export default userSlice.reducer;
