842ef6fddf24e12e506324b7d1c36376f9e1d8da
Mykyta Vostrikov <mykyta.vostrikov@sigma.software>
2022-05-16 15:27:02 +0300

    Minor changes on styles

--- src/components/CountryPage/CountryPage.tsx
+++ src/components/CountryPage/CountryPage.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import styles from "./CountryPage.module.scss";
+// import styles from "./CountryPage.module.scss";
 
 export const CountryPage: FC = () => {
   return <div>Country profile</div>;
--- src/components/UserPage/UserPage.tsx
+++ src/components/UserPage/UserPage.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import styles from "./UserPage.module.scss";
+// import styles from "./UserPage.module.scss";
 
 export const UserPage: FC = () => {
   return <div>User profile</div>;
