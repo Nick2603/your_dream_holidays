9016cf7d7a81de079a61fa7e98d0525dabf96e60
Mykyta Vostrikov <mykyta.vostrikov@sigma.software>
2022-05-18 10:58:50 +0300

    layout fixes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,26 +2,24 @@ import React, { FC } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styles from "./App.module.scss";
 import { Header } from "../Header/Header";
-import { Home } from "../Home/Home";
+import { Home } from "../Pages/HomePage/HomePage";
 import { Footer } from "../Footer/Footer";
-import { NotFoundPage } from "../NotFoundPage";
-import { UserPage } from "../UserPage/UserPage";
-import { CountryPage } from "../CountryPage/CountryPage";
+import { NotFoundPage } from "../Pages/NotFoundPage";
+import { UserPage } from "../Pages/UserPage/UserPage";
+import { CountryPage } from "../Pages/CountryPage/CountryPage";
 
 export const App: FC = () => {
   return (
     <BrowserRouter>
-      <div className={styles.wrapper}>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="*" element={<NotFoundPage />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/userpage" element={<UserPage />} />
-            <Route path="/countrypage" element={<CountryPage />} />
-          </Routes>
-        </main>
-      </div>
+      <Header />
+      <main className={styles.wrapper}>
+        <Routes>
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/userpage" element={<UserPage />} />
+          <Route path="/countrypage/:country" element={<CountryPage />} />
+        </Routes>
+      </main>
       <Footer />
     </BrowserRouter>
   );
