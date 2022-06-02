3a2a1f7106d309729a286905a50c6472e84f9e9f
Mykyta Vostrikov <mykyta.vostrikov@sigma.software>
2022-06-02 13:37:43 +0300

    TI-14 <list of countries fetching from BE and displaying done >

--- src/components/CountrySelect/CountrySelect.tsx
+++ src/components/CountrySelect/CountrySelect.tsx
@@ -1,17 +1,35 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-import { CountryList } from "./CountryList";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 
 export const CountrySelect: FC = () => {
+  const [countryList, setCountryList] = useState([]);
   const [countrySelectorValue, setCountrySelectorValue] = useState<
     string | null
-  >(CountryList[0]);
+  >();
   const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState<Error | null>(null);
   const navigate = useNavigate();
+
+  const handleError = (err: Error) => {
+    setError(err);
+  };
+
+  const getCountryList = () => {
+    return axios
+      .get("http://localhost:5000/countrieslist")
+      .then((res) => res.data)
+      .then((data) => setCountryList(data))
+      .catch(handleError);
+  };
+
+  useEffect(() => {
+    getCountryList();
+  });
 
   const navigateToCountryPage = () => {
     if (countrySelectorValue) {
@@ -20,34 +38,38 @@ export const CountrySelect: FC = () => {
     }
   };
 
-  return (
-    <>
-      <div>
-        <br />
-        <Autocomplete
-          value={countrySelectorValue}
-          onChange={(event: unknown, newValue: string | null) => {
-            setCountrySelectorValue(newValue);
-          }}
-          inputValue={inputValue}
-          onInputChange={(event, newInputValue) => {
-            setInputValue(newInputValue);
-          }}
-          id="controllable-states-demo"
-          options={CountryList}
-          sx={{ width: 300 }}
-          renderInput={(params) => (
-            <TextField {...params} label="Choose your country" />
-          )}
-        />
-      </div>
-      <div style={{ margin: "10px" }}>
-        <Stack spacing={2} direction="row">
-          <Button onClick={navigateToCountryPage} variant="contained">
-            Lets Travel
-          </Button>
-        </Stack>
-      </div>
-    </>
-  );
+  if (error) {
+    return <div>Something went wrong!!!</div>;
+  } else {
+    return (
+      <>
+        <div>
+          <br />
+          <Autocomplete
+            value={countrySelectorValue ? countrySelectorValue : null}
+            onChange={(event: unknown, newValue: string | null) => {
+              setCountrySelectorValue(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            id="controllable-states-demo"
+            options={countryList}
+            sx={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Choose your country" />
+            )}
+          />
+        </div>
+        <div style={{ margin: "10px" }}>
+          <Stack spacing={2} direction="row">
+            <Button onClick={navigateToCountryPage} variant="contained">
+              Lets Travel
+            </Button>
+          </Stack>
+        </div>
+      </>
+    );
+  }
 };
